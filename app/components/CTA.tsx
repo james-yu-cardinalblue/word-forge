3b3fbd4916741752d1e59de97a5a532d8bc6672c
JamesYu <jamesyu608@gmail.com>
2025-02-04 17:06:42 +0800

    CTA

--- app/components/CTA.tsx
+++ app/components/CTA.tsx
@@ -5,8 +5,8 @@ const CTA = () => {
     <section className="bg-blue-700" id="cta">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
-          <span className="block">Ready to streamline your workflow?</span>
-          <span className="block text-blue-200">Start your free trial today.</span>
+          <span className="block">Ready to boost your vocabulary effortlessly?</span>
+          <span className="block text-blue-200">Start building your personal dictionary today!</span>
         </h2>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
